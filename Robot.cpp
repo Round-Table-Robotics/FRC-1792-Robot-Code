fcbf0742ee8f9f6e25cc353f14c5ae7758ab27f7
Donovan <cplusdragon@gmail.com>
2016-08-09 18:50:51 -0500

    Redone Base Code

--- Robot.cpp
+++ Robot.cpp
@@ -1,27 +1,24 @@
-
+#include "SPI.h"
+//#include "Pixy.h"
 #include "WPILib.h"
 
 class Robot: public IterativeRobot
 {
-
+	//Pixy pixy;
 	RobotDrive myRobot; // robot drive system
 	Joystick stick,xbox; // only joystick
 	LiveWindow *lw;
-
-	float speed;
-
-
 
 public:
 	void RobotInit() override {
 	}
 	Robot() :
-	myRobot(0,1),
-	stick(5),
-	xbox(0),
-	lw(LiveWindow::GetInstance()),
+		//These must be enabled in the order declared above
+		myRobot(4,5),	//Originally (1,0)
+		stick(5),		//Needs to be in USB 5
+		xbox(0),
+		lw(LiveWindow::GetInstance())
 
-	speed(0)
 	{
 		myRobot.SetExpiration(0.1); //Code from Example
 		// gyro.InitGyro();
@@ -37,7 +34,6 @@ private:
 	{
 
 	}
-
 	void TeleopInit()		//Initiate Teleoperated
 	{
 
@@ -55,3 +51,4 @@ private:
 	}
 };
 
+START_ROBOT_CLASS(Robot);
